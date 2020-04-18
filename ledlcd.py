39f8fd6e5a3bede50bf2364071c5e2fcdc350faf
Caleb Yates <caldog20@gmail.com>
2020-04-18 14:24:36 -0500

    LCD Scroll with LED

--- ledlcd.py
+++ ledlcd.py
@@ -2,12 +2,15 @@ import I2C_LCD_driver
 from time import sleep
 import wiringpi as wpi
 
-
+str_pad = " " * 16
 wpi.wiringPiSetupGpio()
 wpi.pinMode(21, 1)
 mylcd = I2C_LCD_driver.lcd()
 mylcd.lcd_clear()
 wpi.digitalWrite(21,0)
+
+my_long_string = "Invalid Input, Try Again!"
+my_long_string = str_pad + my_long_string
 
 while True:
     var = int(input("Input:"))
@@ -22,7 +25,11 @@ while True:
         sleep(2)
         mylcd.lcd_clear()
     else:
-        mylcd.lcd_display_string("Try Again, Invalid Input!", 1)
+        for i in range(0, len(my_long_string)):
+            lcd_text = my_long_string[i:(i + 16)]
+            mylcd.lcd_display_string(lcd_text, 1)
+            time.sleep(0.09)
+            mylcd.lcd_display_string(str_pad, 1)
         sleep(0.5)
         wpi.digitalWrite(21,1)
         sleep(0.5)

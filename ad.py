83f9f027c160880eb167553bab03e837b72b0e87
Caleb <caldog20@gmail.com>
2020-04-19 17:23:48 -0500

    Flush

--- ad.py
+++ ad.py
@@ -1,16 +1,17 @@
 import sys
 sys.path.append('libs/')
-import I2C_LCD_driver
-import time
-import Adafruit_DHT as dht
-import pimods
+from pimods import *
 import smbus
 
+# Init smbus for module
 
 bus = smbus.SMBus(1)
-reading =- 1
+
+
 while True:
+    ## Init AN0 on module
     bus.write_byte(0x48, 0)
+    ## Read value of AN0 address
     value = bus.read_byte(0x48)
     print(value)
     time.sleep(1)
